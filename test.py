5cd52175d2350e8398b35ede02441265e29c226d
zhoutk <zhoutk@189.cn>
2019-12-16 15:05:19 +0800

    exclude some db type.

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ with open("list.txt", encoding= 'utf-8') as f:
                                 print(els)
                     for e in els[1:]:
                         dotIndex = e.rfind(".")
-                        if dotIndex > -1 and not(e[:dotIndex] == "info" or e[:dotIndex] == "pic" or e[dotIndex + 1:] == "txt" or e[dotIndex + 1:] == "jpg" or e[dotIndex + 1:] == "jpeg" or e[dotIndex + 1:] == "png" or e[dotIndex + 1:] == "gif"):
+                        if dotIndex > -1 and not(e[:dotIndex] == "info" or e[:dotIndex] == "pic" or e[dotIndex + 1:] == "db" or e[dotIndex + 1:] == "txt" or e[dotIndex + 1:] == "jpg" or e[dotIndex + 1:] == "jpeg" or e[dotIndex + 1:] == "png" or e[dotIndex + 1:] == "gif"):
                             bookCount = bookCount + 1
                             record.append((e[:dotIndex], e[dotIndex + 1:], els[0][:-1], picName,info))
             els = []
