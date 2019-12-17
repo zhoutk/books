a7dec93214ca5039531cc8843494176acb24af80
zhoutk <zhoutk@189.cn>
2019-12-17 10:58:45 +0800

    add field.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 
 conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
 
-with open("list_other.txt", encoding= 'utf-8') as f:
+with open("classified.txt", encoding= 'utf-8') as f:
     line = f.readline()
     els = []
     bookCount = 0
@@ -21,13 +21,15 @@ with open("list_other.txt", encoding= 'utf-8') as f:
                         dotIndex = e.rfind(".")
                         if dotIndex > -1 and not(e.endswith("/")) and not(e[dotIndex + 1:] == "db" or e[dotIndex + 1:] == "jpg" or e[dotIndex + 1:] == "jpeg" or e[dotIndex + 1:] == "png" or e[dotIndex + 1:] == "gif"):
                             bookCount = bookCount + 1
-                            record.append((e[:dotIndex], e[dotIndex + 1:], els[0][:-1], picName,info))
+                            path = els[0][:-1]
+                            rindex = path.rfind("/")
+                            record.append((e[:dotIndex], e[dotIndex + 1:], path, picName,info,path[rindex+1:],"Kindle_Chinese_books_Public"))
             els = []
         line = f.readline()
         if len(record) >= 1000:
                 conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
                 cur = conn.cursor()
-                cur.executemany('insert into `book` (`book_name`,`category`,`path`,`cover`, `abstract`) values (%s,%s,%s,%s,%s)', record)
+                cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`source`) values (%s,%s,%s,%s,%s,%s,%s)', record)
                 conn.commit()
                 cur.close()
                 conn.close()
@@ -37,7 +39,7 @@ with open("list_other.txt", encoding= 'utf-8') as f:
     if len(record) > 0:
         conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
         cur = conn.cursor()
-        cur.executemany('insert into `book` (`book_name`,`category`,`path`,`cover`, `abstract`) values (%s,%s,%s,%s,%s)', record)
+        cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`source`) values (%s,%s,%s,%s,%s,%s,%s)', record)
         conn.commit()
         print(bookCount)
         cur.close()
