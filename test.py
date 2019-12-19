d2b928b4e71a8de379846c56aa98407b0ff8b203
zhoutk <zhoutk@189.cn>
2019-12-19 12:09:50 +0800

    ..

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ with open("kindBookStore.txt", encoding= 'utf-8') as f:
             els.append(line[:-1])
         else:
             if len(els) > 1:
-                if els[0][-1:] == ":" and os.path.exists("/home/zhoutk/" + els[0][:-1]):
+                if els[0][-1:] == ":":
                     path = els[0][:-1]
                     info = ""
                     picName = ""
@@ -24,23 +24,23 @@ with open("kindBookStore.txt", encoding= 'utf-8') as f:
                     c2 = path.find("英文书库")
                     if c1 > -1 or c2 > -1:
                         cindex = c1 if (c1 > -1) else c2
-                        f1 = path.find("/", cindex + 1)
+                        f1 = path.find("/", cindex + 5)
                         if f1 > -1:
-                            classified = path[cindex + 1: f1]
+                            classified = path[cindex + 5: f1]
                         else:
-                            classified = path[cindex + 1:]
+                            classified = path[cindex + 5:]
                     if c1 > -1 and (classified == "小说" or classified == "艺术"):
                         f2 = path.find("/", c1 + 8)
                         if f2 > -1:
                             classified_second = path[c1 + 8:f2]
                         else:
-                            classified_second = path[c1 + 8]
+                            classified_second = path[c1 + 8:]
 
                     for e in els[1:]:
                         dotIndex = e.rfind(".")
                         if dotIndex > -1 and not(e.endswith("/")) and not(e[dotIndex + 1:] == "db" or e[dotIndex + 1:] == "opf" or e[dotIndex + 1:] == "DS_Store"):
                             bookCount = bookCount + 1
-                            record.append((e[:dotIndex], e[dotIndex + 1:], path, picName,info,classified,classified_second,"Kindle_Chinese_books_Public"))
+                            record.append((e[:dotIndex], e[dotIndex + 1:], path, picName,info,classified,classified_second,"KindleBookStore"))
             els = []
         line = f.readline()
         if len(record) >= 10000:
