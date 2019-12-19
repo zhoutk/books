515a6bc2d17fe11714ac79d56fbb9e2a364c0cf7
zhoutk <zhoutk@189.cn>
2019-12-19 11:47:59 +0800

    bookStroe.

--- book.sql
+++ book.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : u14
+ Source Server         : s6-3388
  Source Server Type    : MySQL
- Source Server Version : 50727
- Source Host           : u14.com:3306
+ Source Server Version : 50726
+ Source Host           : 192.168.1.6:3388
  Source Schema         : books
 
  Target Server Type    : MySQL
- Target Server Version : 50727
+ Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 12/12/2019 11:31:41
+ Date: 19/12/2019 10:59:16
 */
 
 SET NAMES utf8mb4;
@@ -23,12 +23,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `category` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `book_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `path` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ext` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `book_name` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `path` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `abstract` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `cover` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `cover` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `classified` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `source` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1253 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 46357 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 
 conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
 
-with open("classified.txt", encoding= 'utf-8') as f:
+with open("kindBookStore.txt", encoding= 'utf-8') as f:
     line = f.readline() 
     els = [] 
     bookCount = 0
@@ -15,21 +15,38 @@ with open("classified.txt", encoding= 'utf-8') as f:
         else:
             if len(els) > 1:
                 if els[0][-1:] == ":" and os.path.exists("/home/zhoutk/" + els[0][:-1]):
+                    path = els[0][:-1]
                     info = ""
                     picName = ""
+                    classified = ""
+                    classified_second = ""
+                    c1 = path.find("中文书库")
+                    c2 = path.find("英文书库")
+                    if c1 > -1 or c2 > -1:
+                        cindex = c1 if (c1 > -1) else c2
+                        f1 = path.find("/", cindex + 1)
+                        if f1 > -1:
+                            classified = path[cindex + 1: f1]
+                        else:
+                            classified = path[cindex + 1:]
+                    if c1 > -1 and (classified == "小说" or classified == "艺术"):
+                        f2 = path.find("/", c1 + 8)
+                        if f2 > -1:
+                            classified_second = path[c1 + 8:f2]
+                        else:
+                            classified_second = path[c1 + 8]
+
                     for e in els[1:]:
                         dotIndex = e.rfind(".")
-                        if dotIndex > -1 and not(e.endswith("/")) and not(e[dotIndex + 1:] == "db" or e[dotIndex + 1:] == "jpg" or e[dotIndex + 1:] == "jpeg" or e[dotIndex + 1:] == "png" or e[dotIndex + 1:] == "gif"):
+                        if dotIndex > -1 and not(e.endswith("/")) and not(e[dotIndex + 1:] == "db" or e[dotIndex + 1:] == "opf" or e[dotIndex + 1:] == "DS_Store"):
                             bookCount = bookCount + 1
-                            path = els[0][:-1]
-                            rindex = path.rfind("/")
-                            record.append((e[:dotIndex], e[dotIndex + 1:], path, picName,info,path[rindex+1:],"Kindle_Chinese_books_Public"))
+                            record.append((e[:dotIndex], e[dotIndex + 1:], path, picName,info,classified,classified_second,"Kindle_Chinese_books_Public"))
             els = []
         line = f.readline()
-        if len(record) >= 1000:
+        if len(record) >= 10000:
                 conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
                 cur = conn.cursor()
-                cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`source`) values (%s,%s,%s,%s,%s,%s,%s)', record)
+                cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`classified_second`,`source`) values (%s,%s,%s,%s,%s,%s,%s,%s)', record)
                 conn.commit()
                 cur.close()
                 conn.close()
@@ -39,7 +56,7 @@ with open("classified.txt", encoding= 'utf-8') as f:
     if len(record) > 0:
         conn = pymysql.connect(host='192.168.1.6', port=3388, user='root', passwd='5LiarZp6', db='books', charset='utf8mb4')
         cur = conn.cursor()
-        cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`source`) values (%s,%s,%s,%s,%s,%s,%s)', record)
+        cur.executemany('insert into `book` (`book_name`,`ext`,`path`,`cover`, `abstract`,`classified`,`classified_second`,`source`) values (%s,%s,%s,%s,%s,%s,%s,%s)', record)
         conn.commit()
         print(bookCount)
         cur.close()
